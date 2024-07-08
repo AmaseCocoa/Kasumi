44facfdac451020254fedd6d2d727257ee75e989
AmaseCocoa <amase.cocoa@gmail.com>
2024-07-08 18:05:37 +0900

    Quit using the assert statement

--- src/kasumi/applications.py
+++ src/kasumi/applications.py
@@ -34,18 +34,18 @@ class Kasumi:
         message to be
         """
         
-        assert scope['type'] == 'http'
-        request = Request(scope, receive)
-        if self.__requests.get(scope['path']):
-            req: dict = self.__requests[scope['path']]
-            if req.get(request.method):
-                func = req.get(request.method)
-                response = await func(request)
-                await response(scope, receive, send)
+        if scope['type'] == 'http':
+            request = Request(scope, receive)
+            if self.__requests.get(scope['path']):
+                req: dict = self.__requests[scope['path']]
+                if req.get(request.method):
+                    func = req.get(request.method)
+                    response = await func(request)
+                    await response(scope, receive, send)
+                else:
+                    await self.__handle_err(request, scope, receive, send, status_code=405)
             else:
-                await self.__handle_err(request, scope, receive, send, status_code=405)
-        else:
-            await self.__handle_err(request, scope, receive, send, status_code=404)
+                await self.__handle_err(request, scope, receive, send, status_code=404)
     
     async def __handle_err(self, request, scope, receive, send, status_code: int=404):
         if self.__err.get(status_code):
